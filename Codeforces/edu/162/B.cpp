4ed62f76a3e4949de1bcd0ecf2c6c33b5d11eb10
Agil <nicholassaraiva16@gmail.com>
2024-08-19 12:59:49 -0300

    organização básica

--- Codeforces/edu/162/B.cpp
+++ Codeforces/edu/162/B.cpp
@@ -1,41 +1,52 @@
-#include<bits/stdc++.h>
-#define _ ios_base::sync_with_stdio(false); cin.tie(NULL);
-#define dbg(x) cerr<<#x<<" = "<<x<<"\n";
-#define pb push_back
-#define all(x) begin(x), end(x)
-#define rall(x) rbegin(x), rend(x)
-#define f first
-#define s second
-#define ll long long
-using namespace std;
+    #include<bits/stdc++.h>
+    #define _ ios_base::sync_with_stdio(false); cin.tie(NULL);
+    #define dbg(x) //cerr<<#x<<" = "<<x<<"\n";
+    #define pb push_back
+    #define all(x) begin(x), end(x)
+    #define rall(x) rbegin(x), rend(x)
+    #define f first
+    #define s second
+    #define ll long long
+    using namespace std;
+    
+    const int MAXN = 1e5+10;
+    
+    int main(){_
+    
+        ll t;
 
-const int MAXN = 1e5+10;
+        cin>>t;
 
-int main(){_
+        while(t--){
+            bool resp = true;
+            ll n,k;
+            cin>>n>>k;
+            vector<ll> health(n), pos(n);
+            vector<ll> HAtPos(n+1,0);
 
-    ll t;
+            for(ll i=0; i<n; i++){
+                cin>>health[i];
+            }
+            for(ll i=0; i<n; i++){
+                cin>>pos[i];
+            }
 
-    cin>>t;
+            for(ll i=0; i<n; i++){
+                HAtPos[abs(pos[i])] += health[i];
+            }
 
-    while(t--){
-        ll n, q;
-        cin>>n>>q;
-        vector<ll> c(n);
-
-        for(ll i=0; i<n; i++){
-            cin>>c[i];
-        }
-
-        while(q--){
-            bool resp;
-            ll l, r;
-            cin>>l>>r;
-
-            
+            ll lft = 0;
+            for(ll i=1; i<=n; i++){
+                lft += k - HAtPos[i];
+                dbg(lft)
+                if(lft < 0){
+                    resp = false;
+                    break;
+                }
+            }
 
             cout<<(resp? "YES": "NO")<<"\n";
         }
+    
+        return 0;
     }
-
-    return 0;
-}
